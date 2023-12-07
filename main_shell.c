a3a15bdc4b18c79599cab827dd60b71c1fd5e5a8
lubaba3 <lubabasalah520@gmail.com>
2023-12-08 00:33:00 +0200

    basic

--- main_shell.c
+++ main_shell.c
@@ -11,8 +11,24 @@ int main(void)
 	size_t size = 0;
 	char **tokens;
 	(void)tokens;
+	 char *line = NULL;
+    size_t len = 0;
+    int read_bytes;
 
 	display();
+    for (;;)
+    {
+			our_print("shell$ ");
+			read_bytes = getline(&line, &len, stdin);
+
+			if (read_bytes == -1)
+			{
+					our_print("\n");
+					our_exit(1, "An error occured!\n");
+					free(line);
+			}
+		}
+
 	our_read(command, size);
 	tokens = cmd_tokenizer(command, delimeter);
 	/* execute_cmd(tokens); */
