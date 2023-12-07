685ad7588e03bddef47927b7bc012dcb0edfb09e
yasirtj <yasirtj11@gmail.com>
2023-12-08 01:38:08 +0200

    edit our_print function

--- print.c
+++ print.c
@@ -10,6 +10,6 @@ void our_print(const char *myprintfuntion)
 	{
 	write(STDOUT_FILENO, myprintfuntion, strlen(myprintfuntion));
 		{
-			write(1, &myprintfuntion, strlen(myprintfuntion));
+			write(1, myprintfuntion, strlen(myprintfuntion));
 		}
 }
